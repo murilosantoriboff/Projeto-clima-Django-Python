fd292b23ddbe139138c69aa7527f557f5bb82e23
Murilo Santori Boff <murilosantori45@gmail.com>
2023-06-09 08:18:49 -0300

    Project Done

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,34 @@
+import requests
 from django.shortcuts import render
+from .models import City
+from .forms import CityForm
 
 def index(request):
-    return render(request, 'index.html')
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=standard&appid=6f9fe42134ece978ca12912a190da734'
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+    
+    cities = City.objects.all()
+
+    weather_data = list()
+
+    for city in cities:
+
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+
+    form = CityForm()
+
+    context = {'weather_data': weather_data, 'form': form}
+
+    return render(request, 'index.html', context)
